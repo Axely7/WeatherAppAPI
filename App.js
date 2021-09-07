2020537f83a253cd184dc39808a04de8365fd5bd
Axel Jimenez <axely7@hotmail.com>
2021-09-06 20:29:20 -0500

    creating form

---
+++ App.js
@@ -0,0 +1,33 @@
+
+import React from 'react';
+import {SafeAreaView,ScrollView,StatusBar,StyleSheet,Text,useColorScheme,View,} from 'react-native';
+import Formulario from './components/Formulario';
+
+
+
+const App = () => {
+  
+  return (
+    <>
+    <View style={styles.app}>
+      <View style={styles.contenido}>
+        <Formulario></Formulario>
+      </View>
+    </View>
+    
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  app:{
+    flex: 1,
+    backgroundColor: 'rgb(71,149,212)',
+    justifyContent: 'center'
+  },
+  contenido:{
+    marginHorizontal: '2.5%'
+  }
+});
+
+export default App;
